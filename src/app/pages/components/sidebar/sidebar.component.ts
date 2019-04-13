f5dc21bb4477461fb26beff745589546114a6363
kazuki.kawabata <>
2019-04-13 20:52:09 +0900

    no message

--- src/app/pages/components/sidebar/sidebar.component.ts
+++ src/app/pages/components/sidebar/sidebar.component.ts
@@ -61,10 +61,7 @@ export class SidebarComponent implements OnInit {
     dom.classList.toggle(this.pushRightClass)
   }
 
-  rltAndLtr() {
-    const dom: any = document.querySelector('body')
-    dom.classList.toggle('rtl')
-  }
+  logout() {}
 
   changeLang(language: string) {}
 
