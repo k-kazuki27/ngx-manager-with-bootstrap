1333faa13140c9c164e572fe9619ca2058909d1c
kazuki.kawabata <>
2019-04-15 00:18:33 +0900

    no message

--- src/app/pages/services/pages.service.ts
+++ src/app/pages/services/pages.service.ts
@@ -1,4 +1,7 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
 import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 import { PagesResolverModule } from './pages-resolver.module'
 
@@ -6,5 +9,9 @@ import { PagesResolverModule } from './pages-resolver.module'
   providedIn: PagesResolverModule
 })
 export class PagesService {
-  constructor() {}
+  isMobile$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches))
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
