d8b2e99a3f7be64637490cac537ddceacf021734
kazuki.kawabata <>
2019-04-17 00:05:02 +0900

    no message

--- src/app/shared/modules/page-header/page-header.component.ts
+++ src/app/shared/modules/page-header/page-header.component.ts
@@ -6,8 +6,10 @@ import { Component, Input, OnInit } from '@angular/core'
   styleUrls: ['./page-header.component.scss']
 })
 export class PageHeaderComponent implements OnInit {
-  @Input() heading: string
-  @Input() icon: string
+  @Input() title: string
+  @Input() titleIcon: string
+  @Input() subTitle: string
+  @Input() subTitleIcon: string
 
   constructor() {}
 
