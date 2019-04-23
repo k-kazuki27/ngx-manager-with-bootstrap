091597b8392ed95ec8db7c09cd8796ac41d05f6b
kazuki.kawabata <>
2019-04-23 23:30:04 +0900

    no message

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -11,6 +11,15 @@ const routes: Routes = [
       {
         path: 'user',
         loadChildren: './user/user.module#UserModule'
+      },
+      {
+        path: '',
+        redirectTo: 'user',
+        pathMatch: 'full'
+      },
+      {
+        path: '**',
+        redirectTo: ''
       }
     ]
   }
