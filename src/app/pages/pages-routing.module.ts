c7d0aa054767d08718de8408001f3dfbc44ec03a
kazuki.kawabata <kazuki.kawabata@fullmarks.co.jp>
2019-04-24 14:36:50 +0900

    modify router2

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -11,15 +11,6 @@ const routes: Routes = [
       {
         path: 'user',
         loadChildren: './user/user.module#UserModule'
-      },
-      {
-        path: '',
-        redirectTo: 'user',
-        pathMatch: 'full'
-      },
-      {
-        path: '**',
-        redirectTo: 'user'
       }
     ]
   }
