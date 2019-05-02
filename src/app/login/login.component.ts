e69b1812bddc98198bcdcf20be8dbc1506469212
kazuki.kawabata <>
2019-05-02 17:08:34 +0900

    no message

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms'
 import { Router } from '@angular/router'
 
 import { routerTransition } from '../router.animations'
+import { LoginApi } from '../shared'
 
 const LOGIN_ERROR = 'ユーザID、もしくはパスワードが間違っています。'
 const REQUIRED = 'ユーザIDとパスワードを入力してください。'
@@ -17,7 +18,11 @@ export class LoginComponent implements OnInit {
   form: FormGroup
   errorMessage: string
 
-  constructor(private router: Router, private fb: FormBuilder) {}
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private loginApi: LoginApi
+  ) {}
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -40,9 +45,23 @@ export class LoginComponent implements OnInit {
       return
     }
 
-    setTimeout(() => {
-      sessionStorage.setItem('isLoggedin', 'true')
-      this.router.navigate(['/'])
-    }, 500)
+    this.loginApi
+      .login(
+        {
+          userId: this.userId.value,
+          password: this.password.value
+        },
+        'body',
+        true
+      )
+      .subscribe(
+        () => {
+          sessionStorage.setItem('isLoggedin', 'true')
+          this.router.navigate(['/'])
+        },
+        () => {
+          this.errorMessage = LOGIN_ERROR
+        }
+      )
   }
 }
