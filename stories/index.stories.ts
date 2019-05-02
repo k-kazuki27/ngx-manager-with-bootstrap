56180dff91c2a57e5be91a2fb5ac4a67452f5569
kazuki.kawabata <>
2019-05-02 12:30:07 +0900

    no message

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,28 +1,30 @@
-import { storiesOf } from '@storybook/angular';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { storiesOf } from '@storybook/angular'
+import { Button } from '@storybook/angular/demo'
+import {
+  RequiredComponent
+} from 'src/app/shared/modules/required/required.component'
 
-import { Welcome, Button } from '@storybook/angular/demo';
-
-storiesOf('Welcome', module).add('to Storybook', () => ({
-  component: Welcome,
-  props: {},
-}));
+storiesOf('RequiredComponent', module).add('Normal', () => ({
+  component: RequiredComponent,
+  props: {}
+}))
 
 storiesOf('Button', module)
   .add('with text', () => ({
     component: Button,
     props: {
-      text: 'Hello Button',
-    },
+      text: 'Hello Button'
+    }
   }))
   .add(
     'with some emoji',
     () => ({
       component: Button,
       props: {
-        text: '😀 😎 👍 💯',
-      },
+        text: '😀 😎 👍 💯'
+      }
     }),
     { notes: 'My notes on a button with emojis' }
   )
@@ -32,16 +34,19 @@ storiesOf('Button', module)
       component: Button,
       props: {
         text: '😀 😎 👍 💯',
-        onClick: action('This was clicked OMG'),
-      },
+        onClick: action('This was clicked OMG')
+      }
     }),
     { notes: 'My notes on a button with emojis' }
-  );
+  )
 
-storiesOf('Another Button', module).add('button with link to another story', () => ({
-  component: Button,
-  props: {
-    text: 'Go to Welcome Story',
-    onClick: linkTo('Welcome'),
-  },
-}));
+storiesOf('Another Button', module).add(
+  'button with link to another story',
+  () => ({
+    component: Button,
+    props: {
+      text: 'Go to Welcome Story',
+      onClick: linkTo('Welcome')
+    }
+  })
+)
