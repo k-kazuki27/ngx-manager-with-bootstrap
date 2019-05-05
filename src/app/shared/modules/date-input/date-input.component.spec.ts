f5c409e0b53e52383f342e0c8ba009237b98f3de
kazuki.kawabata <>
2019-05-05 14:09:33 +0900

    no message

--- src/app/shared/modules/date-input/date-input.component.spec.ts
+++ src/app/shared/modules/date-input/date-input.component.spec.ts
@@ -1,25 +1,30 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
+import { BsDatepickerModule } from 'ngx-bootstrap'
 
-import { DateInputComponent } from './date-input.component';
+import { DateInputComponent } from './date-input.component'
 
 describe('DateInputComponent', () => {
-  let component: DateInputComponent;
-  let fixture: ComponentFixture<DateInputComponent>;
+  let component: DateInputComponent
+  let fixture: ComponentFixture<DateInputComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DateInputComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [DateInputComponent],
+      imports: [ReactiveFormsModule, BsDatepickerModule.forRoot()]
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DateInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(DateInputComponent)
+    component = fixture.componentInstance
+    component.form = new FormGroup({
+      birthday: new FormControl()
+    })
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
