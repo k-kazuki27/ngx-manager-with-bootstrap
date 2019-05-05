a54fe3b17a0bd0234b0727664c49b71ed0f2c986
kazuki.kawabata <>
2019-05-05 13:45:38 +0900

    no message

---
+++ src/app/shared/modules/date-input/date-input.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import {
+  BsDatepickerConfig,
+  BsDatepickerModule,
+  BsLocaleService,
+  defineLocale,
+  jaLocale
+} from 'ngx-bootstrap'
+
+import { DateInputComponent } from './date-input.component'
+
+@NgModule({
+  declarations: [DateInputComponent],
+  imports: [CommonModule, BsDatepickerModule],
+  exports: [DateInputComponent],
+  providers: [
+    BsDatepickerModule.forRoot().providers,
+    { provide: BsDatepickerConfig, useFactory: getDatepickerConfig }
+  ]
+})
+export class DateInputModule {
+  constructor(private bsLocaleService: BsLocaleService) {
+    defineLocale('ja', jaLocale)
+    this.bsLocaleService.use('ja')
+  }
+}
+
+export function getDatepickerConfig(): BsDatepickerConfig {
+  return Object.assign(new BsDatepickerConfig(), {
+    dateInputFormat: 'YYYY-MM-DD',
+    containerClass: 'theme-blue',
+    showWeekNumbers: false
+  })
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { ReactiveFormsModule } from '@angular/forms'
-import {
-  BsDatepickerConfig,
-  BsDatepickerModule,
-  BsLocaleService,
-  defineLocale,
-  jaLocale
-} from 'ngx-bootstrap'
 import { OrderModule } from 'ngx-order-pipe'
 
 import {
   ConfirmModule,
   CustomPaginationModule,
+  DateInputModule,
   DropdownPageLinesModule,
   ErrorLabelModule,
   PageHeaderModule,
@@ -23,7 +17,6 @@ import {
 const modules = [
   CommonModule,
   ReactiveFormsModule,
-  BsDatepickerModule,
   PageHeaderModule,
   DropdownPageLinesModule,
   CustomPaginationModule,
@@ -31,29 +24,13 @@ const modules = [
   RequiredModule,
   ErrorLabelModule,
   QuestionTooltipModule,
+  DateInputModule,
   OrderModule
 ]
 
 @NgModule({
   declarations: [],
   imports: [...modules],
-  exports: [...modules],
-  providers: [
-    BsDatepickerModule.forRoot().providers,
-    { provide: BsDatepickerConfig, useFactory: getDatepickerConfig }
-  ]
+  exports: [...modules]
 })
-export class SharedModule {
-  constructor(private bsLocaleService: BsLocaleService) {
-    defineLocale('ja', jaLocale)
-    this.bsLocaleService.use('ja')
-  }
-}
-
-export function getDatepickerConfig(): BsDatepickerConfig {
-  return Object.assign(new BsDatepickerConfig(), {
-    dateInputFormat: 'YYYY-MM-DD',
-    containerClass: 'theme-blue',
-    showWeekNumbers: false
-  })
-}
+export class SharedModule {}
