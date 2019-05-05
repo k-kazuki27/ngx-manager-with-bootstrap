720e0b038774e92bedde965454986c9848e7826c
kazuki.kawabata <>
2019-05-05 13:36:06 +0900

    no message

--- src/app/pages/user/user-detail/user-detail.component.ts
+++ src/app/pages/user/user-detail/user-detail.component.ts
@@ -14,6 +14,10 @@ import { ConfirmService, UserApi, UserDTO } from 'src/app/shared'
 })
 export class UserDetailComponent implements OnInit, OnDestroy {
   form: FormGroup
+  formGroupClass = 'form-group row align-items-center'
+  labelClass = 'col-sm-3 col-form-label col-form-label-sm'
+  inputClass = 'form-control form-control-sm col-sm-9'
+  errorLabelClass = 'offset-sm-3 col-sm-9'
 
   private id: number
   private onDestroy$ = new Subject()
