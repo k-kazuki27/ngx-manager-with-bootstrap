b9d9a7a5c8c2fcbf4825bec551e85ebf357a21cb
kazuki.kawabata <>
2019-05-06 14:13:18 +0900

    no message

--- src/app/shared/abstracts/abstract-list.spec.ts
+++ src/app/shared/abstracts/abstract-list.spec.ts
@@ -1,3 +1,34 @@
+import { AbstractList } from './abstract-list'
+
+class TestClass {
+  public static create() {
+    return new (class extends AbstractList {
+      search(doPageReset?: boolean): void {}
+      constructor() {
+        super()
+      }
+    })()
+  }
+
+  constructor(private value: string) {}
+}
+
 describe('AbstractList', () => {
-  it('should create an instance', () => {})
+  let target: AbstractList
+
+  beforeEach(() => {
+    target = TestClass.create()
+  })
+
+  it('init', () => {
+    expect(target.currentPage).toBe(1)
+  })
+
+  it('changeItemsPerPage', () => {
+    spyOn(target, 'search')
+    expect(target.itemsPerPage).toBe(25)
+    target.changeItemsPerPage(50)
+    expect(target.itemsPerPage).toBe(50)
+    expect(target.search).toHaveBeenCalled()
+  })
 })
