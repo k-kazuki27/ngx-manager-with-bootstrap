70212b1115070e053961bcaf2829a1b89f7730c9
kazuki.kawabata <kazuki.kawabata@fullmarks.co.jp>
2019-05-24 13:34:51 +0900

    no message

--- src/app/shared/modules/input-dropdown/input-dropdown.component.ts
+++ src/app/shared/modules/input-dropdown/input-dropdown.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-input-dropdown',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-dropdown.component.scss']
 })
 export class InputDropdownComponent implements OnInit {
+  @Input()
+  form!: FormGroup
 
-  constructor() { }
+  items = ['テスト１', 'テスト２', 'テスト３']
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
+
+  select(item: string): void {}
 }
--- src/app/shared/modules/input-dropdown/input-dropdown.module.ts
+++ src/app/shared/modules/input-dropdown/input-dropdown.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
 import { BsDropdownModule } from 'ngx-bootstrap'
 
 import { InputDropdownComponent } from './input-dropdown.component'
 
 @NgModule({
   declarations: [InputDropdownComponent],
-  imports: [CommonModule, BsDropdownModule.forRoot()],
+  imports: [CommonModule, ReactiveFormsModule, BsDropdownModule.forRoot()],
   exports: [InputDropdownComponent]
 })
 export class InputDropdownModule {}
