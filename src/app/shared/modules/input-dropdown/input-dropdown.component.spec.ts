1f32a2d8dcbf1c8a533a2fb715e3c4d0c0cb8f74
kazuki.kawabata <kazuki.kawabata@fullmarks.co.jp>
2019-05-31 13:58:28 +0900

    - exec npm run format:write

--- src/app/shared/modules/input-dropdown/input-dropdown.component.spec.ts
+++ src/app/shared/modules/input-dropdown/input-dropdown.component.spec.ts
@@ -1,25 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { InputDropdownComponent } from './input-dropdown.component';
+import { InputDropdownComponent } from './input-dropdown.component'
 
 describe('InputDropdownComponent', () => {
-  let component: InputDropdownComponent;
-  let fixture: ComponentFixture<InputDropdownComponent>;
+  let component: InputDropdownComponent
+  let fixture: ComponentFixture<InputDropdownComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InputDropdownComponent ]
-    })
-    .compileComponents();
-  }));
+      declarations: [InputDropdownComponent]
+    }).compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InputDropdownComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(InputDropdownComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
