3029b1dffea159cb083566106a9d641f58a17681
kazuki.kawabata <>
2019-06-22 14:10:11 +0900

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { ErrorHandler, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ServiceWorkerModule } from '@angular/service-worker'
 import { NgxSpinnerModule } from 'ngx-spinner'
-import { environment } from 'src/environments/environment'
 
+import { environment } from '../environments/environment'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import {
@@ -15,9 +16,7 @@ import {
   ConfigurationParameters,
   CustomErrorHandlerService,
   CustomInterceptorService
-} from './shared';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment'
+} from './shared'
 
 export function apiConfigFactory(): Configuration {
   const params: ConfigurationParameters = {
@@ -37,7 +36,9 @@ export function apiConfigFactory(): Configuration {
     HttpClientModule,
     NgxSpinnerModule,
     ApiModule.forRoot(apiConfigFactory),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
   providers: [
     { provide: BASE_PATH, useValue: environment.API_BASE_PATH },
