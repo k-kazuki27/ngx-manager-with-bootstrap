3a316dc9fb76c43264fbfdf98547138ebdeec184
kazuki.kawabata <>
2019-07-12 00:37:09 +0900

    - npm update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from './app/app.module'
+import { environment } from './environments/environment'
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(moduleRef => {
+    // const applicationRef = moduleRef.injector.get(ApplicationRef)
+    // const appComponent = applicationRef.components[0]
+    // enableDebugTools(appComponent)
+    // ng.profiler.timeChangeDetection({record: true})
+  })
+
+  .catch(err => console.error(err))
