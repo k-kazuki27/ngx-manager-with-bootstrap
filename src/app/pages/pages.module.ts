07d7ff0b87c716deb78048fedc5881708fb629e4
kazuki.kawabata <>
2019-07-14 19:31:54 +0900

    add select project

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { BsDropdownModule } from 'ngx-bootstrap'
 
-import { CollapseButtonComponent } from './components/collapse-button/collapse-button.component'
+import {
+  CollapseButtonComponent
+} from './components/collapse-button/collapse-button.component'
 import { HeaderComponent } from './components/header/header.component'
 import { SidebarComponent } from './components/sidebar/sidebar.component'
 import { PagesRoutingModule } from './pages-routing.module'
@@ -15,6 +18,11 @@ import { PagesResolverModule } from './services/pages-resolver.module'
     SidebarComponent,
     CollapseButtonComponent
   ],
-  imports: [CommonModule, PagesRoutingModule, PagesResolverModule]
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    PagesResolverModule,
+    BsDropdownModule.forRoot()
+  ]
 })
 export class PagesModule {}
