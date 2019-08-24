f3af340f1c079c5979aebaf87fd6c9cef645f7d5
kazuki.kawabata <kazuki.kawabata@fullmarks.co.jp>
2019-08-24 13:54:29 +0900

    npm update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core'
+import { SwUpdate } from '@angular/service-worker'
 import { detect } from 'detect-browser'
+import { interval } from 'rxjs'
 
 import { RouteHistoryService } from './shared'
 
@@ -10,7 +12,20 @@ import { RouteHistoryService } from './shared'
 })
 export class AppComponent {
   // 履歴保持のため、インスタンス生成だけしておく(RouteHistoryService)
-  constructor(private routeHistoryService: RouteHistoryService) {
+  constructor(
+    private swUpdate: SwUpdate,
+    private routeHistoryService: RouteHistoryService
+  ) {
     console.log(detect())
+
+    // updateChecker
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload()
+      })
+      interval(60 * 60 * 1000).subscribe(() => {
+        this.swUpdate.checkForUpdate()
+      })
+    }
   }
 }
