1704777d2544bcfbdc70d088f6bc370fc33a8524
kazuki.kawabata <>
2019-09-03 00:22:01 +0900

    - fixed size

--- src/app/shared/abstracts/abstract-detail.ts
+++ src/app/shared/abstracts/abstract-detail.ts
@@ -4,8 +4,8 @@ export abstract class AbstractDetail {
   id!: number
   form!: FormGroup
   formGroupClass = 'form-group row align-items-center'
-  labelClass = 'col-sm-3 col-form-label col-form-label-sm'
-  inputClass = 'form-control form-control-sm col-sm-9'
+  labelClass = 'col-sm-3 col-form-label'
+  inputClass = 'form-control col-sm-9'
   errorLabelClass = 'offset-sm-3 col-sm-9'
 
   cancel() {
